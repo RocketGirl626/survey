3d156edd0669b48ca7d978d802d033791bf8ddbf
Bob Ellis & Jeanette Fairless <student@epicodus.com>
2015-05-12 12:17:11 -0700

    Add survey_info erb file and logic to make it work

--- app.rb
+++ app.rb
@@ -8,5 +8,26 @@ require('pg')
 require('pry')
 
 get('/') do
+  @surveys = Survey.all()
   erb(:index)
+end
+
+post('/survey_save/') do
+  @survey_name = params.fetch('survey_name')
+  Survey.create({:name => @survey_name})
+  @surveys = Survey.all()
+  erb(:index)
+end
+
+get('/survey/:id') do
+  @survey = Survey.find(params.fetch('id'))
+  @questions = @survey.questions()
+  erb(:survey_info)
+end
+
+post('/survey/:id') do
+  @survey = Survey.find(params.fetch('id'))
+  @new_question = Question.create({:inquiry => params.fetch('new_question'), :survey_id => params.fetch('id')})
+  @questions = @survey.questions()
+  erb(:survey_info)
 end
